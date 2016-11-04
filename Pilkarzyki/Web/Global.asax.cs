36190426234ba6c57bb2ba8c1ec702c43a93f7e7
migajewski <gajes08@hotmail.com>
2016-11-04 21:45:21 +0100

    autofac configured

--- Pilkarzyki/Web/Global.asax.cs
+++ Pilkarzyki/Web/Global.asax.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using CQRSCore.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -13,6 +17,42 @@ namespace Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            DependencyConfig.RegisterDependencyResolvers();
         }
     }
+
+    public static class DependencyConfig
+    {
+        public static IContainer RegisterDependencyResolvers()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            RegisterDependencyMappingDefaults(builder);
+            RegisterDependencyMappingOverrides(builder);
+            IContainer container = builder.Build();
+            // Set Up MVC Dependency Resolver
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            // Set Up WebAPI Resolver
+            return container;
+        }
+
+        private static void RegisterDependencyMappingDefaults(ContainerBuilder builder)
+        {
+            Assembly webAssembly = Assembly.GetAssembly(typeof(MvcApplication));
+
+            builder.RegisterAssemblyTypes(webAssembly).AsImplementedInterfaces().InstancePerRequest();
+
+            builder.RegisterControllers(webAssembly);
+            builder.RegisterModule(new AutofacWebTypesModule());
+        }
+
+        private static void RegisterDependencyMappingOverrides(ContainerBuilder builder)
+        {
+            var assemblyType = typeof(EventBus).GetTypeInfo();
+
+            builder.RegisterAssemblyTypes(assemblyType.Assembly)
+            .AsImplementedInterfaces()
+            .InstancePerRequest();
+        }
+    }
+
 }
